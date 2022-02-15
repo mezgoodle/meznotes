2e5ef3c43439f683c4e16c088714410c3adc2ca0
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-02-15 09:37:48 +0200

    Create and migrate my first model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Workout(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    exercises = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
