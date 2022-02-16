b5e46fcd57f9225f876b4914725bc464812d2073
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-02-16 17:32:44 +0200

    Create Header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import { Header } from "./components/Header";
 
 function App() {
-  return <div className="App">My app</div>;
+  return (
+    <div className="App">
+      <Header />
+      My app
+    </div>
+  );
 }
 
 export default App;
