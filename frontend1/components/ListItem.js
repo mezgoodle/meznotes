fc058103dd651e7fdfd2c033f33ab991d41716f6
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-02-20 20:15:16 +0200

    Show notes on the page

---
+++ frontend1/components/ListItem.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Link from "next/link";
+
+const ListItem = ({ note }) => {
+  const textLength = 45;
+
+  const getTitle = (note) => {
+    const title = note.body.split("\n")[0];
+    if (title.length > textLength) {
+      return title.slice(0, textLength);
+    }
+    return title;
+  };
+
+  const getContent = (note) => {
+    const title = getTitle(note);
+    let content = note.body.replaceAll("\n", " ");
+    content = content.replaceAll(title, "");
+
+    if (content.length > textLength) {
+      return content.slice(0, textLength) + "...";
+    }
+    return content;
+  };
+
+  const getTime = (note) => {
+    return new Date(note.updated).toLocaleDateString();
+  };
+
+  return (
+    <Link href={`/note/${note.id}`}>
+      <div className="notes-list-item">
+        <h3>{getTitle(note)}</h3>
+        <p>
+          <span>{getTime(note)}</span>
+          {getContent(note)}
+        </p>
+      </div>
+    </Link>
+  );
+};
+
+export default ListItem;
