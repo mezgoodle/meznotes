37efe053a2351b9d4fe8eb05dfbc67b591d2f51c
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-02-21 13:54:39 +0200

    Update tests.py
    Create some tests for views

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
 from .models import Note
+
 
 class NoteModelTest(TestCase):
     @classmethod
@@ -11,3 +13,22 @@ class NoteModelTest(TestCase):
         note = Note.objects.get(id=1)
         expected_object_body = 'Test note'
         self.assertEquals(expected_object_body, note.body)
+
+
+class ApiViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Note.objects.create(body='Test note')
+
+    def test_view_routes(self):
+        resp = self.client.get(reverse('routes'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_notes(self):
+        resp = self.client.get(reverse('notes'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_note(self):
+        resp = self.client.get(reverse('note', kwargs={'pk':1}))
+        self.assertEqual(resp.status_code, 200)
+        print(resp.content)
