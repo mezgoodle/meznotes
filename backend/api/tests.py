73f158179045b22c752a0ebff9f75bfdd056be1d
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-02-21 13:11:28 +0200

    Update tests.py
    Create tests for model Note

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Note
+
+class NoteModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Note.objects.create(body='Test note')
+
+    def test_body_content(self):
+        note = Note.objects.get(id=1)
+        expected_object_body = 'Test note'
+        self.assertEquals(expected_object_body, note.body)
