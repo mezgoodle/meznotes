99144f3b4ef6839d6c69125c44e2def639574a70
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-02-22 09:39:25 +0200

    Create NoteList class based view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework import status
 
 from .models import Note
 from .serializers import NoteSerializer
@@ -41,6 +43,27 @@ def get_routes(request):
         },
     ]
     return Response(routes)
+
+
+class NoteList(APIView):
+    """
+    List all notes, or create a new snippet.
+    """
+    def get(self, request, format=None):
+        notes = Note.objects.all().order_by('-updated')
+        serializer = NoteSerializer(notes, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        data = request.data
+        note = Note.objects.create(
+            body=data['body']
+        )
+        serializer = NoteSerializer(note, many=False)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
